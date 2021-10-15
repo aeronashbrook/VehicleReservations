0e8a9f949f47f0028cdb334ef5977372801583a7
aeronashbrook <42661146+aeronashbrook@users.noreply.github.com>
2021-10-15 14:13:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ function populateVehicles(data, query) {
                 '</td>' +
                 '</tr>' +
                 '</table>' +
-                '<a href="' + button_link + '"><div class="inventory_button">' + 
+                '<a href="' + button_link + ' target="_blank""><div class="inventory_button">' + 
                 '<p>' + button_text + '</p>' +
                 '</div></a>' +
                 '</div>' +
